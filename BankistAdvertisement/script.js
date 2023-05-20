1722e4863e3fd104228a65fe38710dea7c0c4f86
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-05-20 10:22:37 -0400

    Scrolling to next section

--- BankistAdvertisement/script.js
+++ BankistAdvertisement/script.js
@@ -29,3 +29,34 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  console.log('before');
+
+  console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
+
+  console.log('X', s1coords.left, window.pageXOffset);
+  console.log('Y', s1coords.top, window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  console.log('after');
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
