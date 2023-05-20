0171963e9d380d013dcf04379410f6a27ad30e5f
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-05-20 16:09:11 -0400

    Adding code for tab component

--- BankistAdvertisement/exercise.js
+++ BankistAdvertisement/exercise.js
@@ -1,3 +1,5 @@
+// How to add classes and use of getComputedStyle
+
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML =
@@ -20,7 +22,33 @@ const logo = document.querySelector('.nav__logo');
 console.log(logo.dataset.versionNumber);
 console.log(logo.dataset.myname);
 
-logo.classList.add('');
-logo.classList.remove('', '');
-logo.classList.toggle('');
-logo.classList.contains('');
+// logo.classList.add('');
+// logo.classList.remove('', '');
+// logo.classList.toggle('');
+// logo.classList.contains('');
+
+// Event bubbling and capturing
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+);
--- BankistAdvertisement/script.js
+++ BankistAdvertisement/script.js
@@ -35,28 +35,75 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-  console.log(e.target.getBoundingClientRect());
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
 
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
 
-  console.log('before');
+  // console.log('before');
 
-  console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
+  // console.log('Current scroll X/Y', window.pageXOffset, window.pageYOffset);
 
-  console.log('X', s1coords.left, window.pageXOffset);
-  console.log('Y', s1coords.top, window.pageYOffset);
+  // console.log('X', s1coords.left, window.pageXOffset);
+  // console.log('Y', s1coords.top, window.pageYOffset);
 
   // window.scrollTo({
   //   left: s1coords.left + window.pageXOffset,
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: 'smooth',
   // });
-  console.log('after');
+  // console.log('after');
 
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     console.log(this);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   });
+// });
+
+// Tabbed Components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
