2e3f390067d5bd59d2423c3e918e3b027e1cfe32
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-05-27 15:55:43 -0400

    Adding oops code

---
+++ Exercises/constructorfunctions.js
@@ -0,0 +1,22 @@
+"use strict";
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  console.log(this);
+};
+
+let someone = "someone";
+console.log(someone instanceof Person);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+let ritesh = new Person("ritesh", 1988);
+ritesh.calcAge();
+console.log(ritesh);
+console.log(ritesh.__proto__);
+console.log(ritesh.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(ritesh));
+console.log(Person.prototype.isPrototypeOf(Person));
