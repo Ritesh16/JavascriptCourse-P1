10d66ae149c7748f5bb58fc19a19ff1b95bd9b6c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-06-27 21:30:22 -0400

    Adding modules

--- Exercises/Modules/shoppingCart.js
+++ Exercises/Modules/shoppingCart.js
@@ -3,6 +3,11 @@ console.log('Exporting from module');
 const shippingCost = 10;
 const cart = [];
 
+// Blcoking async operation
+// All code below will get blocked.
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+console.log('Finish long running operation');
+
 // Named exports
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
