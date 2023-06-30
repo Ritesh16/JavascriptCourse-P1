523c5dab21bf2016fb2f52bcd825773931419f16
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-06-30 14:52:38 -0400

    Adding code copiled by parcel

---
+++ Exercises/ModernJS/script.js
@@ -0,0 +1,22 @@
+import cloneDeep from 'lodash';
+
+const state = {
+  cart: [
+    { product: 'Bread', quantity: 5 },
+    { product: 'Pizza', quantity: 1 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+
+state.user.loggedIn = false;
+
+const stateCloneDeep = cloneDeep.cloneDeep(state);
+
+console.log(1, stateClone);
+console.log(1, stateCloneDeep);
+
+if (module.hot) {
+  module.hot.accept();
+}
